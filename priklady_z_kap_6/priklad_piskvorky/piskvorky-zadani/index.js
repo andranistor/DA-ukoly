feaf758b03282db04cba694a20963bc720212aa9
Andra <andra.nistor@seznam.cz>
2020-07-05 22:55:08 +0200

    Adding so far two winnig patterns. Calculating the scalar product and evaluating who won. Adding a button to restart the game.Add/remove listener for the square click event.

--- priklady_z_kap_6/priklad_piskvorky/piskvorky-zadani/index.js
+++ priklady_z_kap_6/priklad_piskvorky/piskvorky-zadani/index.js
@@ -1,9 +1,44 @@
 'use strict';
 
-let gamePlan = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-let onTurn = 1;
-const squareElm = document.querySelectorAll('.square');
+let gamePlan = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; // 0 = blank field
 
+// Winnig patterns:
+const patterns = [[1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]];
+
+const squareElm = document.querySelectorAll('.square');
+let onTurn = 1; // 1 = cross, -1 = circle
+
+
+// Calculating the scalar product of arrays and its evaluation
+const score = () => {
+  for (let j = 0; j < patterns.length; j++) {
+    let product = 0;
+    for (let i = 0; i < gamePlan.length; i++) {
+      product += gamePlan[i] * patterns[j][i];
+    }
+
+    if (product === 4) {
+      console.log('Vyhral krizek');
+      squareClickListener(false);
+      break;
+    }
+    else if (product === -4) {
+      console.log('Vyhralo kolecko');
+      squareClickListener(false);
+      break;
+    } else if (!gamePlan.includes(0)) {
+      console.log('Remiza');
+      squareClickListener(false);
+      break;
+    } else {
+      console.log('Hra bezi');
+    }
+  }
+}
+
+
+// After click on a square add a class, change the value in gamePlan and the onTurn number
 const squareClick = (event) => {
   if (gamePlan[event.target.dataset.index] === 0) {
     if (onTurn === 1) {
@@ -18,13 +53,32 @@ const squareClick = (event) => {
       gamePlan[event.target.dataset.index] = onTurn;
       onTurn = 1;
     }
-  }
-}
-
-const squareClickListener = () => {
-  for (let i = 0; i < squareElm.length; i += 1) {
-    squareElm[i].addEventListener('click', squareClick);
+    score();
   }
 };
 
-squareClickListener();
+
+// Adding/removing square click listener
+const squareClickListener = (active) => {
+  for (let i = 0; i < squareElm.length; i += 1) {
+    if (active) {
+      squareElm[i].addEventListener('click', squareClick);
+    } else {
+      squareElm[i].removeEventListener('click', squareClick);
+    }
+  }
+};
+squareClickListener(true);
+
+
+// Btn click to restart a new game, i.e. removing classes and resetting the gamePlan
+const restartGame = () => {
+  gamePlan = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  for (let i = 0; i < squareElm.length; i++) {
+    squareElm[i].classList.remove('square--cross', 'square--circle');
+  }
+  squareClickListener(true);
+};
+
+const btnElm = document.querySelector('.btn__restart');
+btnElm.addEventListener('click', restartGame);
